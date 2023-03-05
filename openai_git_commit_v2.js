8a4c24bfd70598339c2abf836f194b0414973084
Wing CHAN <wingch.apps@gmail.com>
2023-03-05 18:14:54 +0800

    [feat] update role

--- openai_git_commit_v2.js
+++ openai_git_commit_v2.js
@@ -1,6 +1,6 @@
 // #popclip
 // name: OpenAI assistant
-// icon: iconify:logos:openai-icon
+// icon: iconify:simple-icons:openai
 // language: javascript
 // after: preview-result
 // entitlements: [network]
@@ -23,15 +23,15 @@ const data = {
             "content": "You are a helpful development assistant who helps me with the following questions, give the answer directly without any other description, each of the following cases is independent and should not interfere with each other."
         },
         {
-            "role": "system",
+            "role": "user",
             "content": "Case 1: When you receive text in the following format, do not modify the prefix, improve the git commit message \n Format: prefix + git commit message \n prefix is from '[' to ']', The remaining is the git commit message, answer format should be prefix + git commit message"
         },
         {
-            "role": "system",
+            "role": "user",
             "content": "Case 2: When you receive text that starts with '//', it means it is a comment in the code, which helps me to improve the grammar and structure of the comment so that my colleagues can understand it more easily"
         },
         {
-            "role": "system",
+            "role": "user",
             "content": "If the above case also does not match, assuming the text is code, please try to explain, not too long, the answer should start with '//'"
         },
         {
